52faf25e8d878e89e01a5d5f16aa760f76c1281a
Eugene Somov <evso7th@gmail.com>
2025-09-04 12:59:48 +0000

    делай

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 
 import type {NextConfig} from 'next';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -13,25 +11,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-   webpack: (config, { isServer }) => {
-    // We need to copy the Tone.js file to the public folder for the worker
-    if (!config.plugins) {
-        config.plugins = [];
-    }
-    
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.join(__dirname, 'node_modules/tone/build/Tone.js'),
-            to: path.join(__dirname, 'public/assets/vendor/tone/'),
-          },
-        ],
-      })
-    );
-    
-    return config;
   },
 };
 
