aff6268d9ab9fdf79a779dc29d92cebd36c86bb1
Eugene Somov <evso7th@gmail.com>
2025-09-10 10:09:33 +0000

    делай

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,13 +75,23 @@ export default {
             height: '0',
           },
         },
+        'bounce-dot': {
+          '0%, 100%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(-10px)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'bounce-dot': 'bounce-dot 1.4s infinite',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
-
